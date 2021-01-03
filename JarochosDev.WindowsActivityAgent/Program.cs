fd740d9908ba2a30b2393d34a24c8c66b4b0cc52
Fernando David Villafuerte Gertrudis <fernandodavid582@outlook.com>
2021-01-03 17:41:52 -0600

    VILL- Remove name

--- JarochosDev.WindowsActivityAgent/Program.cs
+++ JarochosDev.WindowsActivityAgent/Program.cs
@@ -22,7 +22,6 @@ namespace JarochosDev.WindowsActivityAgent
             var windowsSystemEventService = new WindowsSystemEventService(new WindowsSystemEventListener(logManager));
             windowsSystemEventService.Start();
 
-            Console.WriteLine("Fernando Villafuerte");
             Console.ReadLine();
             windowsSystemEventService.Stop();
         }
