a0536f304355431589b0c704a19b7e0d66bb1cd2
Adrian Ordoñez Tostega <adriantostega@hotmail.com>
2021-01-06 06:46:13 -0800

    ATOS - Using GetHostAddress instead of GetHostByName because that is obsolete. Make setter private for properties in WindowsSystemEventListener.

--- JarochosDev.WindowsActivityAgent.Common/WindowsSystemEventListener.cs
+++ JarochosDev.WindowsActivityAgent.Common/WindowsSystemEventListener.cs
@@ -2,19 +2,19 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.Win32;
-using System.Text;
 using JarochosDev.WindowsActivityAgent.Common.Loggers;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 
 namespace JarochosDev.WindowsActivityAgent.Common
 {
     public class WindowsSystemEventListener
     {
         public LogManager LogManager { get; }
-        public string UserName { get; set; }
-        public string PcName { get; set; }
-        public string IP { get; set; }
+        public string UserName { get; private set; }
+        public string PcName { get; private set; }
+        public string IP { get; private set; }
 
         public WindowsSystemEventListener(LogManager logManager)
         {
@@ -26,13 +26,15 @@ namespace JarochosDev.WindowsActivityAgent.Common
             SystemEvents.PowerModeChanged += SystemEventsOnPowerModeChanged;
             SystemEvents.SessionSwitch += SystemEventsOnSessionSwitch;
             SystemEvents.SessionEnded += SystemEventsOnSessionEnded;
-            PcPropierties();
+            InitializePropierties();
         }
-        private void PcPropierties()
-        {
-            UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\').Last();
-            PcName = Dns.GetHostName();
-            IP = Dns.GetHostByName(PcName).AddressList[0].ToString(); ;
+        private void InitializePropierties()
+        {            
+            UserName = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\').Last();            
+            PcName = Dns.GetHostName();            
+            IP = Dns.GetHostAddresses(Dns.GetHostName())
+                 .Where(IPA => IPA.AddressFamily == AddressFamily.InterNetwork)
+                 .Select(x => x.ToString()).FirstOrDefault();
         }
 
         private void SystemEventsOnSessionEnded( object sender, SessionEndedEventArgs e)
